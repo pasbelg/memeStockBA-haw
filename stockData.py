8b2df90ce3180c4ed75475721182709f80dc92d5
pascal-PC <pascal.belger7@gmail.com>
2021-04-18 16:23:21 +0200

    Starten des Twitter Streams im Hintergrund über Threading ermöglicht und Ausgabe angepasst

--- stockData.py
+++ stockData.py
@@ -5,9 +5,11 @@ import pandas_market_calendars as mcal
 import datetime
 import time
 from databaseFunctions import writeRecord
+from tweetData import tweetMain
 import yfinance as yf
 import pytz
 from tzlocal import get_localzone
+import threading
 
 researchSchedule = pd.read_excel('input/test.xlsx')
 # Funktion die überprüft ob eine übergebene Zeit zwischen zwei übergebenen Zeitpunkten liegt
@@ -48,10 +50,10 @@ def stocksBySchedule(schedule):
                 tickers.append(stock)
     return tickers
 
-def processStockData(stock, datesets):
+def processStockData(stock, datasets):
     # Um die aufgrund der relativen Berechnung auf 0 stehenden Volumendaten entfernen zu können müssen die zu beschaffenden Datensetzt immer 2 Minuten mehr umfassen
     # (insgesamt 4 Minuten, da angebrochene Minute nicht dazu zählt)
-    df = yf.download(tickers=stock, period=str(datesets+3)+'m', interval='1m')
+    df = yf.download(tickers=stock, period=str(datasets+3)+'m', interval='1m', progress = False)
     df = df[:-1]
     df = df[1:]
     df['Ticker'] = stock
@@ -63,11 +65,10 @@ def processStockData(stock, datesets):
         try:
             for minuteData in recordsList:
                 writeRecord(minuteData, 'stockData')
-            print('Daten für "'+stock+'" wurden erfolgreich in die Datenbank geschrieben')
+            print(time.strftime("%Y-%m-%d %H:%M:%S"), '>>', datasets, 'Kursdatensätze für "'+stock+'" wurden erfolgreich gespiechert')
             return True
         except Exception as e:
-            print(e)
-            print(stock+': Fehler beim schreiben in die Datenbank Daten wurden offline gespeichert')
+            print(time.strftime("%Y-%m-%d %H:%M:%S"), '>>', stock+': Fehler beim schreiben der Kursdaten. Fehlercode:', e)
             return False
 
 while True:
@@ -79,31 +80,34 @@ while True:
     #today = datetime.date.today()
     todaysStocks = stocksBySchedule(researchSchedule)
     if todaysStocks:
+        # Setup und Info für den Twitter Stream
+        twitterStream = threading.Thread(name='twitterStream', target=tweetMain, args=[todaysStocks[0]])
+        if len(todaysStocks) > 1:
+            print(time.strftime("%Y-%m-%d %H:%M:%S"), '>> Mehr als eine Aktie eingeplant. Twitterstream nur für', todaysStocks[0], 'aufgebaut')
+
+        # Setup für den Zeitplan der NYSE
         nyse = mcal.get_calendar('NYSE')
         nyseScheduleToday = nyse.schedule(start_date=today, end_date=today)
         backupCounter = dict((stock,1) for stock in todaysStocks)
-        print(backupCounter)
         while timeBetween(getUTC(now), nyseScheduleToday['market_open'][0], nyseScheduleToday['market_close'][0]):
-            print('Der Markt ist geöffnet Datenerhebung gestartet')
+            print(time.strftime("%Y-%m-%d %H:%M:%S"), 'Der Markt ist geöffnet Datenerhebung gestartet')
+            if twitterStream.is_alive() == 0:
+                twitterStream.start()
             for stock in todaysStocks:
                 written = processStockData(stock, backupCounter[stock])
                 if written:
-                    print('Daten wurden gespeichert neuer Schreibvorgang in einer Minute')
                     backupCounter[stock] = 1
-                    print(backupCounter)
                 else:
-                    print('Es gab einen Fehler beim Speichern der Daten Backup wird vorgemerkt')
+                    print(time.strftime("%Y-%m-%d %H:%M:%S"), '>>', stock+': Es konnten keine Einträge gespeichert werden. Aktie wird für Backuplauf vorgemerkt.')
                     backupCounter[stock] += 1
-            #time.sleep(secondsTillNext('minute'))
-            time.sleep(3)
+            print(time.strftime("%Y-%m-%d %H:%M:%S"), '>> Nächster Schreibvorgang in der nächsten vollen Minute')
+            time.sleep(secondsTillNext('minute'))
+            #time.sleep(3)
             
         else:
-            print(nyseScheduleToday)
-            print(getUTC(now))
-            print('Keine neuen Daten verfügbar weil die NYSE geschlossen ist. Es wird bis zur nächsten vollen Minute gewartet')
-            print('Das sind', secondsTillNext('minute'), 'Sekunden')
+            print(time.strftime("%Y-%m-%d %H:%M:%S"), '>> Keine neuen Daten verfügbar weil die NYSE geschlossen ist. Es wird bis zur nächsten vollen Minute gewartet')
+            print(time.strftime("%Y-%m-%d %H:%M:%S"), '>> UTC Zeit:', getUTC(now), 'Heutige Öffnungszeiten (NYSE):', nyseScheduleToday)
             time.sleep(secondsTillNext('minute'))
     else:
-        print('Keine Untersuchung für heute eingeplant es wird bis zur nächsten vollen Stunde gewartet')
-        print(secondsTillNext('hour'))
-        time.sleep(secondsTillNext('hour'))
+        print(time.strftime("%Y-%m-%d %H:%M:%S"), '>> Keine Untersuchung für heute eingeplant es wird bis zum nächsten Tag gewartet')
+        time.sleep(secondsTillNext('day'))
